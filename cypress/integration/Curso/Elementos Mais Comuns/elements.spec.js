443658525edabb09c96ece691dd749ed60e08e57
Vinicius Ribeiro de Oliveira <vinicius.r.oliveira@outlook.com.br>
2025-10-17 15:25:50 -0300

    finalizando sessão 3

--- cypress/integration/Curso/Elementos Mais Comuns/elements.spec.js
+++ cypress/integration/Curso/Elementos Mais Comuns/elements.spec.js
@@ -18,7 +18,7 @@ describe('work with basic elements', () => {
        // cy.get('.facilAchar').should('have.text', 'Cuidado') // have.text não aceita parte do texto, apenas texto completo
     })
 
-    it.only('links', () => {
+    it('links', () => {
         // cy.visit('https://wcaquino.me/cypress/componentes.html');
         cy.get('[href="#"]').click()
         cy.get('#resultado').should('have.text','Voltou!'); //get busca por elemento, id.
@@ -31,5 +31,68 @@ describe('work with basic elements', () => {
         cy.get('#resultado').should('have.text','Voltou!');// contains usa busca pelo texto em tela
 
     })
-  
+
+    it('TextFields', () => {
+        cy.get('#formNome').type('Cypress Test'); // type serve para escrever no campo form
+        cy.get('#formNome').should('have.value','Cypress Test');
+    
+        cy.get('#elementosForm\\:sugestoes') // ao ter : deve adicionar o \\
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+        .type('???')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}') //{backspace} paga o caractere
+            .should('have.value', 'Teste123')
+
+            cy.get('#elementosForm\\:sugestoes') // ao ter : deve adicionar o \\
+            .clear() // limpa o campo
+            .type('Erro{selectall}acerto', {delay:100}) 
+            .should('have.value', 'acerto')
+    })
+
+    it('RadioButton' ,()=>{ //validando a marcação de radio button
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked');
+        cy.get('#formSexoMasc')
+            .should('not.be.checked');
+
+        cy.get("[name='formSexo']")
+            .should('have.length',2) //verificando se há 2 radio buttons com o name formsexo        
+
+    })
+
+    it('CheckBox',()=>{
+        cy.get('#formComidaPizza')
+            .click()
+            .should('be.checked');
+
+        cy.get('[name=formComidaFavorita]')
+            .click({multiple:true}) // caso não tenha o parametro multiple dará erro pois há mais de 1 elemento com o mesmo name.
+        
+
+        cy.get('#formComidaPizza')
+            .should('not.be.checked');
+            
+        cy.get('#formComidaCarne')
+            .should('be.checked');
+    })
+
+    it.only('Combo', ()=>{
+        cy.get('[data-test=dataEscolaridade]')
+            .select('2o grau completo')
+            .should('have.value','2graucomp');
+
+            //TODO VALDAR AS OPÇÕES DE COMBO
+    })
+
+    it.only('ComboMultiplo', ()=>{
+        cy.get('[data-testid=dataEsportes]')
+            .select(['natacao','Corrida']); // quando combo multiplo deve ser usado o value
+
+          //  TODO VALIDAR OPÇÕES SELECIONADAS DO COMBO MULTIPLO
+    })
 })
